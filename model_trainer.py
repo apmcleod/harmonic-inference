76af90756c4d0310c2c97cf2a023079dbbfcd528
Andrew McLeod <apmcleod@gmail.com>
2020-06-10 15:46:24 +0200

    Implemented scheduler saving, early stopping

--- model_trainer.py
+++ model_trainer.py
@@ -1,10 +1,12 @@
 import os
 import sys
+import shutil
 
 import torch
 import torch.nn as nn
 
 from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 from harmonic_inference_data import pad_and_collate_samples
 
@@ -18,10 +20,11 @@ class ModelTrainer():
     
     def __init__(self, model, train_dataset=None, valid_dataset=None, test_dataset=None,
                  device=torch.device("cpu"), seed=None,
-                 batch_size=64, valid_batch_size=None, num_epochs=100,
-                 optimizer=None, scheduler=None, criterion=None,
+                 batch_size=64, valid_batch_size=None, num_epochs=100, early_stopping=20,
+                 optimizer=None, scheduler=None, schedule_var=None, criterion=None,
                  log_every=1, log_file=None,
-                 save_every=10, save_dir='.', save_prefix='checkpoint'):
+                 save_every=10, save_dir='.', save_prefix='checkpoint',
+                 resume=None):
         """
         Create a new ModelTrainer object.
         
@@ -54,11 +57,18 @@ class ModelTrainer():
         num_epochs : int
             The number of epochs to train for.
             
+        early_stopping : int
+            Stop before num_epochs if there has been no improvement in validation loss for this many epochs.
+            
         optimizer : torch.optim
             The optimizer to use during training.
             
         scheduler : torch.scheduler
             The scheduler to use during training (if any).
+            
+        schedule_var : string
+            The name of the variable to be passed to the scheduler (if any). Can be:
+            valid_loss, valid_acc, train_loss, or train_acc.
             
         criterion : torch.criterion
             The loss criterion to use during training and evaluation.
@@ -77,6 +87,9 @@ class ModelTrainer():
             
         save_prefix : string
             A prefix to use when saving the model checkpoint, by default.
+            
+        resume : string
+            The path of a checkpoint file to load from, optionally.
         """
         if valid_batch_size is None:
             valid_batch_size = batch_size
@@ -96,17 +109,56 @@ class ModelTrainer():
         self.model.to(self.device)
         
         self.num_epochs = num_epochs
+        self.epoch = 0
+        self.early_stopping = early_stopping
+        
+        self.best_valid_acc = 0
+        self.best_epoch = -1
         
         self.optimizer = optimizer
         self.scheduler = scheduler
+        self.schedule_var = schedule_var
         self.criterion = criterion
+        
+        if type(self.scheduler) is ReduceLROnPlateau:
+            valid_vars = ['train_loss', 'train_acc', 'valid_loss', 'valid_acc']
+            assert self.schedule_var in valid_vars, (
+                "If scheduler is ReduceLROnPlateau, schedule_var must be one of " +
+                str(valid_vars)
+            )
         
         self.log_every = log_every
         self.log_file = sys.stdout if log_file is None else log_file
         
         self.save_every = save_every
-        self.save_dir = save_dir
-        self.save_prefix = save_prefix
+        self.save_dir = save_dir if save_dir is not None and save_dir != '' else '.'
+        self.save_prefix = save_prefix if save_prefix is not None else 'checkpoint'
+        os.makedirs(self.save_dir, exist_ok=True)
+        
+        if resume is not None:
+            assert os.path.isfile(resume), f"Resume file {resume} not found (or not a file)."
+            
+            checkpoint = torch.load(resume)
+            
+            self.epoch = checkpoint['epoch']
+            self.num_epochs += self.epoch
+            
+            self.best_valid_acc = checkpoint['best_valid_acc']
+            self.best_epoch = checkpoint['best_epoch']
+            
+            if self.scheduler is not None:
+                if 'scheduler' in checkpoint:
+                    scheduler.load_state_dict(checkpoint['scheduler'])
+                else:
+                    print("Resume file has no scheduler information. Not loading it.", file=sys.stderr)
+            elif 'scheduler' in checkpoint:
+                print("Resume file has scheduler information saved, but scheduler is None. Not loading it.",
+                      file=sys.stderr)
+                
+            self.model.load_state_dict(checkpoint['model'])
+            self.model.to(self.device)
+            
+            self.optimizer.load_state_dict(checkpoint['optimizer'])
         
         
         
@@ -127,22 +179,44 @@ class ModelTrainer():
         """
         Train the loaded model on the loaded data.
         """
-        for epoch in range(self.num_epochs):
+        for self.epoch in range(self.epoch, self.num_epochs):
             # Training
             train_loss, train_acc = self.iteration(train=True)
 
             # Validation
             valid_loss, valid_acc = self.iteration(train=False)
+            
+            if self.scheduler is not None:
+                if type(self.scheduler) is ReduceLROnPlateau:
+                    self.scheduler.step({
+                        'train_loss': train_loss,
+                        'train_acc': train_acc,
+                        'valid_loss': valid_loss,
+                        'valid_acc': valid_acc
+                    }[self.schedule_var])
+                else:
+                    self.scheduler.step()
 
             # Log printing
-            if epoch % self.log_every == 0:
-                self.log([train_loss, train_acc, valid_loss, valid_acc])
+            if self.epoch % self.log_every == 0:
+                self.log([self.epoch, train_loss, train_acc, valid_loss, valid_acc])
 
             # Save checkpoints
-            if epoch % self.save_every == 0:
-                self.save(epoch=epoch)
+            is_best = valid_acc > self.best_valid_acc
+            if is_best:
+                self.best_valid_acc = valid_acc
+                self.best_epoch = self.epoch
+            if self.epoch % self.save_every == 0:
+                self.save(is_best=is_best, valid_acc=valid_acc)
                 
-        self.save(epoch=epoch)
+            # Early stopping
+            if self.early_stopping is not None:
+                if self.best_epoch + self.early_stopping <= self.epoch:
+                    print("Early stopping")
+                    break
+                
+        # Always save last epoch
+        self.save(is_best=is_best, valid_acc=valid_acc)
     
     
     
@@ -209,39 +283,40 @@ class ModelTrainer():
     
     
     
-    def save(self, epoch=None, save_path=None):
+    def save(self, is_best=False, valid_acc=None):
         """
         Save the currently loaded model to a file. By default,
-        {self.save_dir}/{self.save_prefix}_e{epoch}.model. If save_path is given, the model is saved
-        to save_path.
+        {self.save_dir}/{self.save_prefix}.pth.tar.
         
         Parameters
         ----------
-        epoch : int
-            The epoch to write into the file name. If not given, '_e{epoch}' is removed from the filename.
+        is_best : bool
+            If True, this copies the resulting checkpoint file to best.pth.tar in the same directory.
             
-        save_path : string
-            The file to save the model to, if given.
+        valid_acc : float
+            The validation accuracy. Saved in the checkpoint file.
             
         Returns
         -------
         save_path : string
             The file the model checkpoint was saved to.
         """
-        if save_path is None:
-            save_dir = '.' if self.save_dir is None else self.save_dir
-            save_prefix = 'checkpoint' if self.save_prefix is None else self.save_prefix
-            save_filename = f'{save_prefix}_e{epoch}.model' if epoch is not None else f'{save_prefix}.model'
-            save_path = os.path.join(save_dir, save_filename)
-        else:
-            save_dir = os.path.dirname(save_path)
-        if save_dir != '':
-            os.makedirs(save_dir, exist_ok=True)
+        save_path = os.path.join(self.save_dir, f'{self.save_prefix}.pth.tar')
         
         # Save model on cpu
-        torch.save(self.model.cpu(), save_path)
+        torch.save({
+            'epoch': self.epoch + 1,
+            'valid_acc': valid_acc,
+            'best_valid_acc': self.best_valid_acc,
+            'best_epoch': self.best_epoch,
+            'model': self.model.cpu().state_dict(),
+            'optimizer' : self.optimizer.state_dict(),
+            'scheduler': self.scheduler.state_dict() if self.scheduler is not None else None
+        }, save_path)
         self.model.to(self.device)
         
-        print(f"Model saved to {save_path}")
+        if is_best:
+            shutil.copyfile(save_path, os.path.join(self.save_dir, 'best.pth.tar'))
+
         return save_path
     
