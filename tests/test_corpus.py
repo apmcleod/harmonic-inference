5c12a46efc7eb6cacfc63aa9fbde90b6127b077e
Andrew McLeod <apmcleod@gmail.com>
2020-08-25 16:48:44 +0200

    Corpus reading tested

--- tests/test_corpus.py
+++ tests/test_corpus.py
@@ -16,24 +16,45 @@ CHORDS_TSV = TSV_BASE / 'chords.tsv'
 NOTES_TSV = TSV_BASE / 'notes.tsv'
 MEASURES_TSV = TSV_BASE / 'measures.tsv'
 
-files_df = read_dump(FILES_TSV, index_col=0)
-measures_df = read_dump(MEASURES_TSV)
-chords_df = read_dump(CHORDS_TSV, low_memory=False)
-notes_df = read_dump(NOTES_TSV)
+files_dfs = {
+    'default': None
+}
 
-removed_repeats = cu.remove_repeats(measures_df, remove_unreachable=True)
+measures_dfs = {
+    'default': None,
+    'removed': None,
+}
 
-chords_df_removed = cu.remove_unmatched(chords_df, removed_repeats)
-chords_df = chords_df.drop(chords_df.loc[(chords_df.numeral == '@none') | chords_df.numeral.isnull()].index)
-offsets_chords_df = cu.add_chord_metrical_data(chords_df_removed, removed_repeats)
+chords_dfs = {
+    'default': None,
+    'removed': None,
+    'dropped': None,
+    'offsets': None
+}
 
-notes_df_removed = cu.remove_unmatched(notes_df, removed_repeats)
-offsets_notes_df = cu.add_note_offsets(notes_df_removed, removed_repeats)
-merged_notes_df = cu.merge_ties(offsets_notes_df)
+notes_dfs = {
+    'default': None,
+    'removed': None,
+    'offsets': None,
+    'merged': None
+}
+
+
+def test_files():
+    global files_dfs, measures_dfs, chords_dfs, notes_dfs
+    files_dfs['default'] = read_dump(FILES_TSV, index_col=0)
 
 
 def test_measures():
-    for file_id, piece_df in removed_repeats.groupby('file_id'):
+    global files_dfs, measures_dfs, chords_dfs, notes_dfs
+    measures_dfs['default'] = read_dump(MEASURES_TSV)
+    measures_dfs['removed'] = cu.remove_repeats(measures_dfs['default'], remove_unreachable=True)
+
+    for df in measures_dfs.values():
+        assert all(df.index.get_level_values(0).isin(files_dfs['default'].index))
+
+    # Well-formedness
+    for file_id, piece_df in measures_dfs['removed'].groupby('file_id'):
         piece_df = piece_df.copy()
 
         assert len(piece_df.loc[piece_df['next'].isnull()]) == 1, "Not exactly 1 mc ends."
@@ -48,105 +69,40 @@ def test_measures():
         assert len(piece_df.loc[piece_df.next <= piece_df.mc]) == 0
 
 
-def test_add_chord_metrical_data():
-    # Check well-formedness, structure, size, etc
-    assert offsets_chords_df.mc_next.dtype == 'Int64'
-    assert isinstance(offsets_chords_df.onset_next.values[0], Fraction)
-    assert isinstance(offsets_chords_df.duration.values[0], Fraction)
-    assert all(offsets_chords_df.index == chords_df_removed.index)
-    assert set(offsets_chords_df.columns) - set(chords_df_removed.columns) == set(['mc_next',
-                                                                                  'onset_next',
-                                                                                  'duration'])
-    assert len(set(chords_df_removed.columns) - set(offsets_chords_df.columns)) == 0
-    assert len(offsets_chords_df.loc[offsets_chords_df.mc_next.isnull()]) == 0
-    assert len(offsets_chords_df.loc[offsets_chords_df.onset_next.isnull()]) == 0
+def test_chords():
+    global files_dfs, measures_dfs, chords_dfs, notes_dfs
+    chords_dfs['default'] = read_dump(CHORDS_TSV, low_memory=False)
+    chords_dfs['removed'] = cu.remove_unmatched(chords_dfs['default'], measures_dfs['removed'])
+    chords_dfs['dropped'] = chords_dfs['removed'].drop(
+        chords_dfs['removed'].loc[(chords_dfs['removed'].numeral == '@none') |
+                                  chords_dfs['removed'].numeral.isnull()].index
+    )
+    chords_dfs['offsets'] = cu.add_chord_metrical_data(chords_dfs['dropped'],
+                                                       measures_dfs['removed'])
 
-    # Check accuracy
-    for file_id, piece_df in offsets_chords_df.loc[:20].groupby('file_id'):
-        last_measure = removed_repeats.loc[file_id].loc[removed_repeats.loc[file_id].next.isnull()]
-        last_chord = piece_df.iloc[-1]
-        assert last_chord.mc_next == last_measure.mc.values[0]
-        assert last_chord.onset_next == (last_measure.act_dur + last_measure.offset).values[0]
-        assert ru.get_range_length((last_chord.mc, last_chord.onset),
-                                   (last_chord.mc_next, last_chord.onset_next),
-                                   removed_repeats.loc[file_id]) == last_chord.duration
+    for df in chords_dfs.values():
+        assert all(df.index.get_level_values(0).isin(files_dfs['default'].index))
 
-        for (_, prev_chord), (_, next_chord) in zip(piece_df.iloc[:-1].iterrows(),
-                                                    piece_df.iloc[1:].iterrows()):
-            assert prev_chord.mc_next == next_chord.mc
-            assert prev_chord.onset_next == next_chord.onset
-            assert ru.get_range_length((prev_chord.mc, prev_chord.onset),
-                                       (next_chord.mc, next_chord.onset),
-                                       removed_repeats.loc[file_id]) == prev_chord.duration, (
-                f'{file_id}: {prev_chord}'
-            )
+    assert all(chords_dfs['default'].mc.isin(measures_dfs['default'].mc))
+    assert all(chords_dfs['removed'].mc.isin(measures_dfs['removed'].mc))
+    assert all(chords_dfs['dropped'].mc.isin(measures_dfs['removed'].mc))
+    assert all(chords_dfs['offsets'].mc.isin(measures_dfs['removed'].mc))
+    assert all(chords_dfs['offsets'].mc_next.isin(measures_dfs['removed'].mc))
 
 
-def test_get_notes_during_chord():
-    num_tests = 1000
-    indexes = np.random.randint(low=0, high=len(offsets_chords_df), size=num_tests)
-    return_sizes = []
-    return_non_onsets = []
+def test_notes():
+    global files_dfs, measures_dfs, chords_dfs, notes_dfs
+    notes_dfs['default'] = read_dump(NOTES_TSV)
+    notes_dfs['removed'] = cu.remove_unmatched(notes_dfs['default'], measures_dfs['removed'])
+    notes_dfs['offsets'] = cu.add_note_offsets(notes_dfs['removed'], measures_dfs['removed'])
+    notes_dfs['merged'] = cu.merge_ties(notes_dfs['offsets'])
 
-    for i in indexes:
-        chord = offsets_chords_df.iloc[i]
-        notes = cu.get_notes_during_chord(chord, offsets_notes_df)
-        return_sizes.append(len(notes))
-        return_non_onsets.append(0)
-        for _, note in notes.iterrows():
-            if pd.isna(note.overlap):
-                # Note onset is not before chord
-                assert (note.mc, note.onset) >= (chord.mc, chord.onset)
-                # Note offset is not after chord
-                assert (chord.mc_next, chord.onset_next) >= (note.offset_mc, note.offset_beat)
+    for df in notes_dfs.values():
+        assert all(df.index.get_level_values(0).isin(files_dfs['default'].index))
 
-            elif note.overlap == -1:
-                return_non_onsets[-1] += 1
-                # Note onset is before chord
-                assert (note.mc, note.onset) < (chord.mc, chord.onset)
-                # Note offset is not after chord
-                assert (chord.mc_next, chord.onset_next) >= (note.offset_mc, note.offset_beat)
-
-            elif note.overlap == 0:
-                return_non_onsets[-1] += 1
-                # Note onset is before chord
-                assert (note.mc, note.onset) < (chord.mc, chord.onset)
-                # Note offset is after chord
-                assert (chord.mc_next, chord.onset_next) < (note.offset_mc, note.offset_beat)
-
-            elif note.overlap == 1:
-                # Note onset is not before chord
-                assert (note.mc, note.onset) >= (chord.mc, chord.onset)
-                # Note offset is after chord
-                assert (chord.mc_next, chord.onset_next) < (note.offset_mc, note.offset_beat)
-
-            else:
-                assert False, "Invalid overlap value returned: " + str(note.overlap)
-
-    for list_index, i in enumerate(indexes):
-        chord = offsets_chords_df.iloc[i]
-        notes = cu.get_notes_during_chord(chord, offsets_notes_df, onsets_only=True)
-        assert len(notes) == return_sizes[list_index] - return_non_onsets[list_index], (
-            "Length of returned df incorrect with onsets_only"
-        )
-        for _, note in notes.iterrows():
-            if pd.isna(note.overlap):
-                # Note onset is not before chord
-                assert (note.mc, note.onset) >= (chord.mc, chord.onset)
-                # Note offset is not after chord
-                assert (chord.mc_next, chord.onset_next) >= (note.offset_mc, note.offset_beat)
-
-            elif note.overlap == -1:
-                assert False, "onsets_only returned an overlap -1"
-
-            elif note.overlap == 0:
-                assert False, "onsets_only returned an overlap 0"
-
-            elif note.overlap == 1:
-                # Note onset is not before chord
-                assert (note.mc, note.onset) >= (chord.mc, chord.onset)
-                # Note offset is after chord
-                assert (chord.mc_next, chord.onset_next) < (note.offset_mc, note.offset_beat)
-
-            else:
-                assert False, "Invalid overlap value returned: " + str(note.overlap)
+    assert all(notes_dfs['default'].mc.isin(measures_dfs['default'].mc))
+    assert all(notes_dfs['removed'].mc.isin(measures_dfs['removed'].mc))
+    assert all(notes_dfs['offsets'].mc.isin(measures_dfs['removed'].mc))
+    assert all(notes_dfs['offsets'].offset_mc.isin(measures_dfs['removed'].mc))
+    assert all(notes_dfs['merged'].mc.isin(measures_dfs['removed'].mc))
+    assert all(notes_dfs['merged'].offset_mc.isin(measures_dfs['removed'].mc))
